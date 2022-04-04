a59026302919f7aa1ed00163948d077b374a9d08
etoy22 <ethanleider@gmail.com>
2022-04-04 17:31:03 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,39 @@
+
+def initSBox(box):
+    for i in range(256):
+        box.append(i)
+
+
+def ksa (key,box):
+    j = 0
+    for i in range (256):
+        j = (j + box[i] + key[i % len(key)]) % 256
+        swap(box,i,j)
+    
+def ksa_string(plain,key):
+    encode = bytes(key, 'utf-8')
+    box = []
+    initSBox(box)
+    ksa(encode,box)
+    keyStream = ""
+    keyStream = prga(plain, box, keyStream)
+    print(keyStream)
+
+
+def prga(plain,box,keyStream):
+    i = 0
+    j = 0
+    for i in range(len(plain)):
+        i = (i + 1) % 256
+        j = (j + box[i]) % 256
+        swap(box,i,j)
+        keyStream += chr(box[(box[i] + box[j]) % 256])
+    return keyStream
+    
+def swap(box,a,b):
+    temp = box[a]
+    box[a] = box[b]
+    box[b] = temp
+
+ksa_string("plain","key")
+
