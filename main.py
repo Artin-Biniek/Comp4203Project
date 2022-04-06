0bbbb26f66af79e7ee823ee1f5b6f4af616ecf53
A-Creative-Username569 <71418717+A-Creative-Username569@users.noreply.github.com>
2022-04-06 00:07:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ plaintext = "test"
 test = rc4Encrypt(plaintext,key)
 print(test)
 print(rc4Decrypt(test,key))
+
+#Testing FMS.py function
+keySteam=""
+output=""
+box=""
+plain="Test message"
+
+
+ksa(key,plain)
+print(prga(plain, box, keyStream, output))
