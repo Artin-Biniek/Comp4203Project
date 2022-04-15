646b035159dba6621f4799520cf1c0f8755124c2
etoy22 <ethanleider@gmail.com>
2022-04-15 11:20:35 -0400

    Update FMS.py

--- FMS.py
+++ FMS.py
@@ -80,7 +80,6 @@ def Output (plain):
         for k in range(256):
             iv[2] = k
             sessionKey = iv + key
-            # print(str(sessionKey))
             box = []
             
             ksa(sessionKey,box)
@@ -93,26 +92,48 @@ def Output (plain):
 
             cipherByte = (int("aa", 16)) ^ keyStreamByte
             values.append([str(iv[0]),str(iv[1]),str(iv[2]),str(cipherByte)])
-    return values,sessionKey
+    return values
     
 def RecoverKey(values):
-    iv = [0] * 3
     keyLength = int(values[-1][0]) - 2
+    key = [None] * 3
     for A in range(keyLength):
-        prob = [0]*256
-        for i in range(len(values)):
-            iv[0] = values[i][0]
-            iv[1] = values[i][1]
-            iv[2] = values[i][2]
-        
-        
+        prob = [0] * 256
+        for row in values:
+            key[0] = int(row[0])
+            key[1] = int(row[1])
+            key[2] = int(row[2])
+
+            j = 0
+            box = []
+            for i in range(256):
+                box.append(i) 
+
+            for i in range(A + 3):
+                j = (j + box[i] + key[i]) % 256
+                box[i],box[j] = box[j],box[i]
+                if i == 1:
+                    original0 = box[0]
+                    original1 = box[1]
+
+            i = A + 3
+            z = box[1]
+            if z + box[z] == A + 3:
+                if (original0 != box[0] or original1 != box[1]):
+                    continue
+                keyStreamByte = int(row[3]) ^ int("aa", 16)
+                keyByte = (keyStreamByte - j - box[i]) % 256
+                prob[keyByte] += 1
+            higherPossibility = prob.index(max(prob))
+        key.append(higherPossibility)
+
+    userInput = key[3:]
+    result = [format(key, 'x') for key in userInput]
+    rawkey = ''.join(result).upper()
+    print(rawkey)
     
 
-
-
- 
-v,sessionKey = Output("F23456")
-print(sessionKey)
+v = Output("EB5723")
 if (v == 0):
     print(v)
     print("Value Not A Hex")
